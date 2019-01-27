f4850679f33435024e19f73c0466588f8a13fa78
PontiacGTX <pontiacsunfiregt@gmail.com>
2019-01-27 12:10:28 -0400

    Update dl.cs

--- dl.cs
+++ dl.cs
@@ -192,17 +192,15 @@ namespace dl
             {
                 filePath = Rename(filePath);
             }
-
+             Console.WriteLine($"Downloading File");
             if (url.Contains("ftp"))
             {
-                    Console.WriteLine($"Downloading File");
                     DownloadFTP(filePath);
             }
             else
             {
                 try
                 {
-                    Console.WriteLine($"Downloading File");
                     client.Headers.Add("User-Agent", "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
                     client.DownloadFile(url, filePath);
                 }
