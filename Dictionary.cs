9a72af06fefe48c732448c41844c3aa6bef7de06
PontiacGTX <pontiacsunfiregt@gmail.com>
2019-02-02 22:37:00 -0400

    Update Dictionary.cs

--- Dictionary.cs
+++ Dictionary.cs
@@ -32,7 +32,7 @@ namespace dl
             LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
             OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
             SOFTWARE.
-             
+             https://github.com/samuelneff/MimeTypeMap
              */
         #endregion
 
