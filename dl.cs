3a0d046e78f37a81720b6e995074b7fdda54817b
PontiacGTX <pontiacsunfiregt@gmail.com>
2019-02-10 10:59:26 -0400

    Update dl.cs

--- dl.cs
+++ dl.cs
@@ -584,6 +584,7 @@ namespace dl
                     client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36");
 
                     client.DownloadFile(url, filePath);
+                    WebException = false;
 
                 }
                 catch (WebException ex)
